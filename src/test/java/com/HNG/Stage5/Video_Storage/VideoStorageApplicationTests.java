b8d9be4cb6e49b9987ddb4742bc248225f87d337
OkechukwuOdo <okechukwuoodo@gmail.com>
2023-09-29 13:11:11 +0100

    created files

---
+++ src/test/java/com/HNG/Stage5/Video_Storage/VideoStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HNG.Stage5.Video_Storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
